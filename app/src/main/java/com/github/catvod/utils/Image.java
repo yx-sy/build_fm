eab96f3c6a31106b48ab0925e9340692f04c304d
yxsy <56529455+yx-sy@users.noreply.github.com>
2024-10-19 16:17:15 +0800

    Update Image.java

--- app/src/main/java/com/github/catvod/utils/Image.java
+++ app/src/main/java/com/github/catvod/utils/Image.java
@@ -2,9 +2,9 @@ package com.github.catvod.utils;
 
 public class Image {
 
-    public static final String FOLDER = "https://gitee.com/yx-sy/img/raw/main/cus/folder.png";
-    public static final String VIDEO = "https://gitee.com/yx-sy/img/raw/main/cus/video.png";
-    public static final String PUSH = "https://gitee.com/yx-sy/img/raw/main/cus/push.png";
+    public static final String FOLDER = "https://gitee.com/yx-sy/imgnp/raw/main/cus/folder.png";
+    public static final String VIDEO = "https://gitee.com/yx-sy/imgnp/raw/main/cus/video.png";
+    public static final String PUSH = "https://gitee.com/yx-sy/imgnp/raw/main/cus/push.png";
 
     public static String getIcon(boolean folder) {
         return folder ? FOLDER : VIDEO;
